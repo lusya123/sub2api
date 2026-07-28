08d5e1e6a983448f1fbd2c4ecd001c543f669dae
lusya123 <11420774703@qq.com>
2026-07-29 01:08:45 +0800

    chore(ent): regenerate group reasoning policy bindings

--- backend/ent/mutation.go
+++ backend/ent/mutation.go
@@ -25125,7 +25125,7 @@ func (m *GroupMutation) Type() string {
 // order to get all numeric fields that were incremented/decremented, call
 // AddedFields().
 func (m *GroupMutation) Fields() []string {
-	fields := make([]string, 0, 51)
+	fields := make([]string, 0, 53)
 	if m.created_at != nil {
 		fields = append(fields, group.FieldCreatedAt)
 	}
